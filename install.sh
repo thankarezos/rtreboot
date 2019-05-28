0672fe37c70d0e957434a152ac1ccdff78059fb0
thankarezos <thankarezos@gmail.com>
2019-05-28 19:50:02 +0300

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 shname=$(basename -- "$0")
 read -p  "Want to install router command? [y/n]: " a
 if [ $a = y ];then
+	sudo chmod +x router
 	sudo cp router /usr/bin
 	if [ $? -eq 0 ];then
 		echo "Succsessfully Installed"
