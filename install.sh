cd16707de384ecc37bf7bf56d6919ba55666dbf7
thankarezos <thankarezos@gmail.com>
2019-05-28 19:44:15 +0300

    new

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+shname=$(basename -- "$0")
+read -p  "Want to install router command? [y/n]: " a
+if [ $a = y ];then
+	sudo cp router /usr/bin
+	if [ $? -eq 0 ];then
+		echo "Succsessfully Installed"
+	fi
+else
+	echo "$shname: Aborted"
+fi
